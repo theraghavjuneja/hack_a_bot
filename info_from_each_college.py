edeb9a46aa9276c46e93066ae2afdb3a084a6f66
RAGHAV JUNEJA <raghavjuneja2391@gmail.com>
2024-01-19 10:56:30 +0530

    Handled selenium handshake

--- info_from_each_college.py
+++ info_from_each_college.py
@@ -22,7 +22,18 @@ def extract_element(class_name):
         text_list.append(heading.text)
     
     return text_list
+def get_non_matching_labels(driver):
+    find_class = driver.find_elements(By.CLASS_NAME, 'MuiFormLabel-root.MuiInputLabel-root.MuiInputLabel-animated.MuiFormLabel-colorPrimary.MuiInputLabel-root.MuiInputLabel-animated.css-12po370')
 
+    # Assuming there's only one element, get its text
+    label_result = ''
+    if find_class:
+        label_result = find_class[0].text
+
+    all_labels = driver.find_elements(By.CLASS_NAME, 'MuiTypography-root.MuiTypography-body1.css-w4oa02')
+    non_matching_labels = [element.text for element in all_labels if element.text != label_result]
+
+    return non_matching_labels
 
 headings=extract_element('MuiFormLabel-root.MuiInputLabel-root.MuiInputLabel-animated.MuiFormLabel-colorPrimary.MuiInputLabel-root.MuiInputLabel-animated.css-2vzt7s') 
 start_dates_list=extract_element('MuiTypography-root.MuiTypography-body1.css-1udcvx7')
@@ -31,23 +42,26 @@ for heading in headings:
 print()
 for dates in start_dates_list:
     print(dates)
+
+
+things=get_non_matching_labels()
+for thing in things:
+    print(thing)
+
 # <label class="MuiFormLabel-root MuiInputLabel-root MuiInputLabel-animated MuiFormLabel-colorPrimary MuiInputLabel-root MuiInputLabel-animated css-12po370">Bennett University, Greater Noida </label>
 
 # 3rd one has a different approach
-find_class=driver.find_elements(By.CLASS_NAME,'MuiFormLabel-root.MuiInputLabel-root.MuiInputLabel-animated.MuiFormLabel-colorPrimary.MuiInputLabel-root.MuiInputLabel-animated.css-12po370')
-# corrsponding to one element only
-label_result=''
-for element in find_class:
-    label_result=element.text
+# find_class=driver.find_elements(By.CLASS_NAME,'MuiFormLabel-root.MuiInputLabel-root.MuiInputLabel-animated.MuiFormLabel-colorPrimary.MuiInputLabel-root.MuiInputLabel-animated.css-12po370')
+# # corrsponding to one element only
+# label_result=''
+# for element in find_class:
+#     label_result=element.text
 
-print()
-print()
-print()
-all_labels=driver.find_elements(By.CLASS_NAME,'MuiTypography-root.MuiTypography-body1.css-w4oa02')
-for element in all_labels:
-    if(element.text==label_result):
-        pass
-    else:
-        print(element.text)
+# all_labels=driver.find_elements(By.CLASS_NAME,'MuiTypography-root.MuiTypography-body1.css-w4oa02')
+# for element in all_labels:
+#     if(element.text==label_result):
+#         pass
+#     else:
+#         print(element.text)
 
 
